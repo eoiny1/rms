b69286583bfdf317b81ab14fce0a9b5d1d68c10c
eoin <eoiny1@yahoo.com>
2020-03-06 17:05:59 -0500

    adding rms_send grid to allow joins

---
+++ Model/ResourceModel/RmsSend/Grid/Collection.php
@@ -0,0 +1,172 @@
+<?php
+
+
+namespace Neon\Rms\Model\ResourceModel\RmsSend\Grid;
+
+use Magento\Framework\Api\Search\SearchResultInterface;
+use Magento\Framework\Search\AggregationInterface;
+use Neon\Rmstest\Model\ResourceModel\RmsSend\Collection as RmsSendCollection;
+
+/**
+ * Class Collection
+ *
+ * @package Neon\Rmstest\Model\ResourceModel\RmsSend
+ */
+class Collection extends RmsSendCollection implements SearchResultInterface
+{
+
+    /**
+     * @var AggregationInterface
+     */
+    protected $aggregations;
+  
+    protected $_date;
+  
+  /**
+     * @param \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
+     * @param \Magento\Framework\Event\ManagerInterface $eventManager
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\EntityManager\MetadataPool $metadataPool
+     * @param string $mainTable
+     * @param string $eventPrefix
+     * @param string $eventObject
+     * @param string $resourceModel
+     * @param string $model
+     * @param \Magento\Framework\DB\Adapter\AdapterInterface|string|null $connection
+     * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+      \Magento\Framework\Stdlib\DateTime\TimezoneInterface $date,
+        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        $mainTable,
+        $eventPrefix,
+        $eventObject,
+        $resourceModel,
+        $model = 'Magento\Framework\View\Element\UiComponent\DataProvider\Document',
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
+        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
+    ) {
+      $this->_date = $date;
+      parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $connection,
+            $resource
+        );
+        $this->_eventPrefix = $eventPrefix;
+        $this->_eventObject = $eventObject;
+        $this->_init($model, $resourceModel);
+        $this->setMainTable($mainTable);
+    }
+  
+  
+  
+protected function _initSelect()
+{
+    parent::_initSelect();
+  
+        $this->getSelect()->joinLeft(
+                ['rmssendorder' => $this->getTable('neon_rmstest_rmssendorder')],
+                'main_table.rmssend_id = rmssendorder.rms_send_id AND main_table.sent_type = 1',
+               ['rmssendorder.order_id as order_id']
+            );
+  
+        
+      $this->getSelect()->joinLeft(
+                ['rmssendcr' => $this->getTable('neon_rmstest_rmssendcr')],
+                'main_table.rmssend_id = rmssendcr.rms_send_id AND main_table.sent_type = 2',
+               ['rmssendcr.cr_id as cr_id']
+            );
+  
+  
+}
+  
+  
+ 
+    /**
+     * @return AggregationInterface
+     */
+    public function getAggregations()
+    {
+        return $this->aggregations;
+    }
+ 
+    /**
+     * @param AggregationInterface $aggregations
+     * @return $this
+     */
+    public function setAggregations($aggregations)
+    {
+        $this->aggregations = $aggregations;
+    }
+ 
+    /**
+     * Get search criteria.
+     *
+     * @return \Magento\Framework\Api\SearchCriteriaInterface|null
+     */
+    public function getSearchCriteria()
+    {
+        return null;
+    }
+ 
+    /**
+     * Set search criteria.
+     *
+     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+     * @return $this
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setSearchCriteria(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria = null)
+    {
+        return $this;
+    }
+ 
+    /**
+     * Get total count.
+     *
+     * @return int
+     */
+    public function getTotalCount()
+    {
+        return $this->getSize();
+    }
+ 
+    /**
+     * Set total count.
+     *
+     * @param int $totalCount
+     * @return $this
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setTotalCount($totalCount)
+    {
+        return $this;
+    }
+ 
+    /**
+     * Set items list.
+     *
+     * @param \Magento\Framework\Api\ExtensibleDataInterface[] $items
+     * @return $this
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setItems(array $items = null)
+    {
+        return $this;
+    }
+  
+  
+  
+}
+
