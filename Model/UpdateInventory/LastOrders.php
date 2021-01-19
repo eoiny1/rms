e1135e49b22d748218519385713d4b34b5324a3b
eoiny1 <eoiny1@yahoo.com>
2021-01-19 15:00:38 -0500

    removing exclude skus hack. It's back woking now

--- Model/UpdateInventory/LastOrders.php
+++ Model/UpdateInventory/LastOrders.php
@@ -61,9 +61,6 @@ class LastOrders extends \Magento\Framework\Model\AbstractModel {
   */
   protected  function getLastOrderItems() {
     
-    //Hack for the moment
-    return  $sku_exclude_array = [];
-    
     $orderItems = $this->_orderItemRepository->getList($this->buildSearchCriteria())->getItems();
     
     $sku_exclude_array = [];
